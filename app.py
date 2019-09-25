8fae19e1dce00d863cb5c8f83fdabf4ddff4bd12
Ya-Jung, Lu <farewellXD@users.noreply.github.com>
2019-09-25 21:52:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -98,12 +98,12 @@ home_layout = html.Div([
             dbc.Card([
                 dbc.CardBody([
                     html.H2("eBird Taiwan秋季大亂鬥", className="card-title home-card-title"),
-                    html.P("和其他eBirder組隊來PK，共有3隊 任你選，灰面鵟鷹隊、黑面琵鷺隊 與小辮鴴隊，在10月上傳觀察清單、 鳥種數最多的隊伍，全隊隊員可獲 得一次望遠鏡抽獎機會。",className="card-text home-card-content"),
+                    html.P("和其他eBirder組隊來PK，共有3隊任你選，ET灰面鵟鷹隊、ET黑面琵鷺隊、ET小辮鴴隊，在10月上傳最多觀察清單以及記錄到最多種鳥!",className="card-text home-card-content"),
                     html.Br(),html.Br(),html.Br(),
                 ]),
                 dbc.CardFooter(html.Div([
                         html.Div("2019/10/01-31",className="mr-auto date-range-text"),
-                        dbc.Button("詳情活動說明",id="modal-entry-1", className="modal-entry border-0 bg-transparent")
+                        dbc.Button("詳細活動說明",id="modal-entry-1", className="modal-entry border-0 bg-transparent")
                     ],className="d-flex align-items-center"), className="border-0 bg-transparent")
                 ]),
             dbc.Card([
@@ -114,18 +114,18 @@ home_layout = html.Div([
                 ]),
                 dbc.CardFooter(html.Div([
                         html.Div("2019/10/01-31",className="mr-auto date-range-text"),
-                        dbc.Button("詳情活動說明",id="modal-entry-2", className="modal-entry border-0 bg-transparent")
+                        dbc.Button("詳細活動說明",id="modal-entry-2", className="modal-entry border-0 bg-transparent")
                     ],className="d-flex align-items-center"), className="border-0 bg-transparent")
                 ]),
             dbc.Card([
                 dbc.CardBody([
-                    html.H2("10/19全球觀鳥大日", className="card-title home-card-title"),
-                    html.P("去年的觀鳥大日集結全世界3萬多位 eBirder的力量，短短24小時共記錄 6,331種鳥類。台灣則記錄242種鳥類 ，送出372份清單，今年的結果究竟 會如何?趕緊安排10月19日出門賞鳥， 一起努力送清單吧!",className="card-text home-card-content"),
+                    html.H2("10/19十月觀鳥大日", className="card-title home-card-title"),
+                    html.P("10月19日十月觀鳥大日(October Global Big Day)!去年的觀鳥大日集結全世界3萬多位eBirder的力量，短短24小時共記錄6,331種鳥類。台灣則記錄242種鳥類，送出372份清單，今年的結果究竟會如何?趕緊安排10月19日出門賞鳥，一起努力送清單吧!",className="card-text home-card-content"),
                     html.Br(),html.Br(),html.Br(),
                 ]),
                 dbc.CardFooter(html.Div([
                         html.Div("2019/10/01-31",className="mr-auto date-range-text"),
-                        dbc.Button("詳情活動說明",id="modal-entry-3", className="modal-entry border-0 bg-transparent")
+                        dbc.Button("詳細活動說明",id="modal-entry-3", className="modal-entry border-0 bg-transparent")
                     ],className="d-flex align-items-center"), className="border-0 bg-transparent")
                 ]),           
         ], className="home-card-deck"),
@@ -144,13 +144,15 @@ home_layout = html.Div([
         ## Contents are not checked yet ##
         * 挑戰時間：2019.10.01-2019.10.31
         * 挑戰方式：
-            * 填寫要加入的隊伍，可隨時加入，自由選擇隊伍，但每人只能加入一隊。
-            * 清單上傳時請分享到隊伍帳號（ET灰面鵟鷹隊、ET黑面琵鷺隊、ET小辮鴴隊)，才能列入挑戰紀錄。
+            * 在10/15之前填寫要加入的隊伍及至少上傳一份記錄清單，每人只能加入一隊，不能更換隊伍。
+            * 清單上傳時請分享到隊伍帳號（ET灰面鵟鷹隊、ET黑面琵鷺隊、ET小辮鴴隊)，才能列入挑戰紀錄，需在10/31前完成分享。
             * 紀錄清單必須持續時間超過3分鐘，鳥種數量沒有以「X」代替的完整紀錄清單。
             * 賞鳥動態和eBird鳥訊快報需設為公開，才能列入紀錄。        
         * 獎項：
-            * 上傳鳥種數最多  
-            * 上傳清單數最多
+            * 記錄鳥種數最多或上傳紀錄清單最多的隊伍，全隊隊員可各獲得一次望遠鏡(歐帝生光學BRITEC R 10X42雙筒望遠鏡)抽獎機會。  
+        * 得獎名單公布：
+            * 2019.11.24, 於eBird Taiwan FB社團進行直播抽獎，公布得獎名單。
+        
         ''')),className="modal-body"),
         dbc.ModalFooter(dbc.Button("關閉", id="close-modal-1", className="ml-auto")),
         ],id="modal-1",size="xl",centered=True),
@@ -158,31 +160,32 @@ home_layout = html.Div([
         dbc.ModalHeader("關渡觀鳥大日",className="modal-title"),
         dbc.ModalBody(dcc.Markdown(dedent('''
         ## Contents are not checked yet ##
-        * 挑戰時間：2019.10.01-2019.10.31
+        * 挑戰時間：2019.10.19, 0:00-24:00
         * 挑戰方式：
-            * 填寫要加入的隊伍，可隨時加入，自由選擇隊伍，但每人只能加入一隊。
-            * 清單上傳時請分享到隊伍帳號（ET灰面鵟鷹隊、ET黑面琵鷺隊、ET小辮鴴隊)，才能列入挑戰紀錄。
-            * 紀錄清單必須持續時間超過3分鐘，鳥種數量沒有以「X」代替的完整紀錄清單。
-            * 賞鳥動態和eBird鳥訊快報需設為公開，才能列入紀錄。        
+            * 選擇活動範圍內就近熱點上傳紀錄清單，清單種類不限，但必須設為公開。
+            * 所有紀錄都要在10月20日中午12:00前上傳到eBird，才能被計算在關渡觀鳥大日的統計結果裡。
         * 獎項：
             * 上傳鳥種數最多  
             * 上傳清單數最多
+            * 上傳鳥隻數最多
+        * 獎項：
+            * 貓頭鷹浴巾組 _八色鳥咖啡出品*1
+            * 歐帝生2019望遠鏡新品5折折價卷*1
+        * 得獎名單公布：2019.10.20, 15:00於關渡鳥博eBird攤位公布
         ''')),className="modal-body"),
         dbc.ModalFooter(dbc.Button("關閉", id="close-modal-2", className="ml-auto")),
         ],id="modal-2",size="xl",centered=True),
     dbc.Modal([
-        dbc.ModalHeader("10/19全球觀鳥大日",className="modal-title"),
+        dbc.ModalHeader("10/19十月觀鳥大日",className="modal-title"),
         dbc.ModalBody(dcc.Markdown(dedent('''
         ## Contents are not checked yet ##
-        * 挑戰時間：2019.10.01-2019.10.31
+        * 挑戰時間：2019.10.19
         * 挑戰方式：
-            * 填寫要加入的隊伍，可隨時加入，自由選擇隊伍，但每人只能加入一隊。
-            * 清單上傳時請分享到隊伍帳號（ET灰面鵟鷹隊、ET黑面琵鷺隊、ET小辮鴴隊)，才能列入挑戰紀錄。
-            * 紀錄清單必須持續時間超過3分鐘，鳥種數量沒有以「X」代替的完整紀錄清單。
-            * 賞鳥動態和eBird鳥訊快報需設為公開，才能列入紀錄。        
+            * 在十月觀鳥大日完成一份鳥種數量沒有以「X」代替的完整紀錄清單。        
         * 獎項：
-            * 上傳鳥種數最多  
-            * 上傳清單數最多
+            * 望遠鏡(歐帝生光學BRITEC R 10X42雙筒望遠鏡)抽獎機會 
+        * 得獎名單公布：
+            * 2019.11.24, 於eBird Taiwan FB社團進行直播抽獎，公布得獎名單。
         ''')),className="modal-body"),
         dbc.ModalFooter(dbc.Button("關閉", id="close-modal-3", className="ml-auto")),
         ],id="modal-3",size="xl",centered=True),
